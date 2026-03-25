6d02658309e02fa75766545051b10b55ccd591b7
shizeying <shizeying@joyme.sg>
2026-03-25 11:33:57 +0800

    revert(relay): restore controller/relay.go behavior from 922999b8 and align tests

--- controller/relay.go
+++ controller/relay.go
@@ -96,9 +96,6 @@ func Relay(c *gin.Context, relayFormat types.RelayFormat) {
 				string(newAPIError.GetErrorType()),
 				string(newAPIError.GetErrorCode()),
 			)
-			if newAPIError.StatusCode == http.StatusTooManyRequests {
-				newAPIError.SetMessage("当前请求较多，请稍后重试")
-			}
 			newAPIError.SetMessage(common.MessageWithRequestId(newAPIError.Error(), requestId))
 			switch relayFormat {
 			case types.RelayFormatOpenAIRealtime:
@@ -240,15 +237,9 @@ func Relay(c *gin.Context, relayFormat types.RelayFormat) {
 		relayInfo.LastError = newAPIError
 		service.MarkChannelBreakerFailure(channel, newAPIError)
 
-		shouldRetryCurrent := shouldRetry(c, newAPIError, common.RetryTimes-retryParam.GetRetry())
-		processChannelError(
-			c,
-			*types.NewChannelError(channel.Id, channel.Type, channel.Name, channel.ChannelInfo.IsMultiKey, common.GetContextKeyString(c, constant.ContextKeyChannelKey), channel.GetAutoBan()),
-			newAPIError,
-			true,
-		)
+		processChannelError(c, *types.NewChannelError(channel.Id, channel.Type, channel.Name, channel.ChannelInfo.IsMultiKey, common.GetContextKeyString(c, constant.ContextKeyChannelKey), channel.GetAutoBan()), newAPIError)
 
-		if !shouldRetryCurrent {
+		if !shouldRetry(c, newAPIError, common.RetryTimes-retryParam.GetRetry()) {
 			break
 		}
 	}
@@ -366,7 +357,7 @@ func shouldRetry(c *gin.Context, openaiErr *types.NewAPIError, retryTimes int) b
 	return operation_setting.ShouldRetryByStatusCode(code)
 }
 
-func processChannelError(c *gin.Context, channelError types.ChannelError, err *types.NewAPIError, notifyExternal bool) {
+func processChannelError(c *gin.Context, channelError types.ChannelError, err *types.NewAPIError) {
 	logger.LogError(c, fmt.Sprintf("channel error (channel #%d, status code: %d): %s", channelError.ChannelId, err.StatusCode, err.Error()))
 	// 不要使用context获取渠道信息，异步处理时可能会出现渠道信息不一致的情况
 	// do not use context to get channel info, there may be inconsistent channel info when processing asynchronously
@@ -411,9 +402,7 @@ func processChannelError(c *gin.Context, channelError types.ChannelError, err *t
 		model.RecordErrorLog(c, userId, channelId, modelName, tokenName, err.MaskSensitiveErrorWithStatusCode(), tokenId, useTimeSeconds, false, userGroup, other)
 	}
 
-	if notifyExternal {
-		service.NotifyMonitorCallError(c, channelError, err)
-	}
+	service.NotifyMonitorCallError(c, channelError, err)
 
 }
 
@@ -571,16 +560,10 @@ func RelayTask(c *gin.Context) {
 
 		if !taskErr.LocalError {
 			service.MarkTaskChannelBreakerFailure(channel, taskErr)
-			shouldRetryCurrent := shouldRetryTaskRelay(c, channel.Id, taskErr, common.RetryTimes-retryParam.GetRetry())
 			processChannelError(c,
 				*types.NewChannelError(channel.Id, channel.Type, channel.Name, channel.ChannelInfo.IsMultiKey,
 					common.GetContextKeyString(c, constant.ContextKeyChannelKey), channel.GetAutoBan()),
-				types.NewOpenAIError(taskErr.Error, types.ErrorCodeBadResponseStatusCode, taskErr.StatusCode),
-				true)
-			if !shouldRetryCurrent {
-				break
-			}
-			continue
+				types.NewOpenAIError(taskErr.Error, types.ErrorCodeBadResponseStatusCode, taskErr.StatusCode))
 		}
 
 		if !shouldRetryTaskRelay(c, channel.Id, taskErr, common.RetryTimes-retryParam.GetRetry()) {
